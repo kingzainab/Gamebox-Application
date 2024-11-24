30d31de666c2e7cd30995bec5318a2eda14e7e19
saad-1719 <chsaad1407@gmail.com>
2024-11-24 12:33:33 +0500

    music manager updated

--- app/src/main/java/com/zsinnovations/gamebox/MainActivity.java
+++ app/src/main/java/com/zsinnovations/gamebox/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        musicManager.resumeMusic(this);
+        musicManager.resumeMusic();
     }
 
     @Override
