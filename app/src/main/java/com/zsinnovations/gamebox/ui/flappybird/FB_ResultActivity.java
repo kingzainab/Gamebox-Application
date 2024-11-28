e365af050719389468ecab809a18ee8374d8494c
kingzainab <zainabasif14@gmail.com>
2024-11-29 00:43:38 +0500

    Result Activity Initialized.

--- app/src/main/java/com/zsinnovations/gamebox/ui/flappybird/FB_ResultActivity.java
+++ app/src/main/java/com/zsinnovations/gamebox/ui/flappybird/FB_ResultActivity.java
@@ -1,8 +1,18 @@
 package com.zsinnovations.gamebox.ui.flappybird;
 
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,17 +20,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.zsinnovations.gamebox.R;
 
-public class FB_ResultActivity extends AppCompatActivity {
+public class FB_ResultActivity extends AppCompatActivity
+{
+    private TextView textViewResultInfo, textViewMyScore, textViewHighScore;
+    private Button buttonAgain;
+    private int score, highScore;
+
+    private SharedPreferences sharedPreferences;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fb_result);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViewHighScore = findViewById(R.id.textViewHighScore);
+        textViewMyScore = findViewById(R.id.textViewMyScore);
+        textViewResultInfo = findViewById(R.id.textViewResultInfo);
+        buttonAgain = findViewById(R.id.buttonAgain);
     }
 }
