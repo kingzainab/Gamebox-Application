f0e068a42968b4068e9355ceb73ee97e69d4755e
kingzainab <zainabasif14@gmail.com>
2024-12-19 03:17:30 +0500

    Rotation Handler Added in kt

---
+++ app/src/main/java/com/zsinnovations/gamebox/ui/tetris/activity/InfoActivity.kt
@@ -0,0 +1,12 @@
+package com.zsinnovations.gamebox.ui.tetris.activity;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zsinnovations.gamebox.ui.tetris.R
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
