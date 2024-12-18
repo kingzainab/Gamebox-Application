4185136b8e382dda0069552b2366e2a51ef33391
kingzainab <zainabasif14@gmail.com>
2024-12-19 03:56:13 +0500

    Added necessary dependencies T_T

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id ("kotlin-kapt")
 }
 
 android {
@@ -36,20 +37,53 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation ("com.google.code.gson:gson:2.8.9")
-    implementation("de.hdodenhof:circleimageview:3.1.0")
+    implementation(libs.gson)
+    implementation(libs.circleimageview)
     implementation(libs.firebase.crashlytics.buildtools)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.gridlayout)
     implementation(libs.core.ktx)
+    implementation(libs.datastore.preferences) // Added dependency
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    implementation (libs.org.jetbrains.kotlin.kotlin.stdlib)
+    implementation (libs.core.ktx.v132)
+    implementation (libs.appcompat.v120)
+    implementation (libs.material.v130)
+    implementation (libs.constraintlayout.v204)
+    testImplementation (libs.junit)
+    androidTestImplementation (libs.junit.v112)
+    androidTestImplementation (libs.espresso.core.v330)
+
+    // Datastore
+    implementation (libs.datastore.preferences.v100alpha06)
+
+    // Coroutines
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+
+    // Coroutine Lifecycle Scopes
+    implementation (libs.lifecycle.viewmodel.ktx)
+    implementation (libs.lifecycle.runtime.ktx)
+
+    // Room components
+    implementation (libs.room.ktx)
+    kapt (libs.androidx.room.compiler)
+    androidTestImplementation (libs.androidx.room.testing)
+
+    // Card View
+    implementation (libs.androidx.cardview)
+
+    //Lottie
+    implementation (libs.lottie)
+
+    //Konfetti
+    implementation (libs.konfetti)
 }
