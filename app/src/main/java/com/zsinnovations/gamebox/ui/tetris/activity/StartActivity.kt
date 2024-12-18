057c904f96570d88e8a9054e664bd854fe51a713
kingzainab <zainabasif14@gmail.com>
2024-12-19 03:14:08 +0500

    Info and Setting UI + Basic Implementation

---
+++ app/src/main/java/com/zsinnovations/gamebox/ui/tetris/activity/StartActivity.kt
@@ -0,0 +1,34 @@
+package com.zsinnovations.gamebox.ui.tetris.activity;
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zsinnovations.gamebox.ui.tetris.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        binding.NewGameButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.settingButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.HighScoreButton.setOnClickListener {
+            val intent = Intent(this@StartActivity, HighscoreActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
