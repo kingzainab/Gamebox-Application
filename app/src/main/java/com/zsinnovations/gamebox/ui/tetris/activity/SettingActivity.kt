8aab110b81b12512d587c0da7a64e67afc67be8a
kingzainab <zainabasif14@gmail.com>
2024-12-19 03:17:58 +0500

    Level Manager

---
+++ app/src/main/java/com/zsinnovations/gamebox/ui/tetris/activity/SettingActivity.kt
@@ -0,0 +1,94 @@
+package com.zsinnovations.gamebox.ui.tetris.activity;
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SeekBar
+import androidx.lifecycle.lifecycleScope
+import com.zsinnovations.gamebox.ui.tetris.constants.BlockColorTheme
+import com.zsinnovations.gamebox.ui.tetris.database.BlockThemeManager
+import com.zsinnovations.gamebox.ui.tetris.database.LevelManager
+import com.zsinnovations.gamebox.ui.tetris.databinding.ActivitySettingBinding
+import kotlinx.coroutines.launch
+
+class SettingActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySettingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val blockThemeManager = BlockThemeManager(applicationContext)
+        val levelManager = LevelManager(applicationContext)
+        lifecycleScope.launch {
+            if (levelManager.getInitialLevel() == null) {
+                binding.seekBar.progress = 0
+                binding.levelInstant.text = "1"
+            } else {
+                binding.seekBar.progress = levelManager.getInitialLevel()!! - 1
+                binding.levelInstant.text = levelManager.getInitialLevel()!!.toString()
+            }
+        }
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.levelInstant.text = (progress + 1).toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                lifecycleScope.launch {
+                    if (seekBar != null) {
+                        levelManager.setInitalLevel(seekBar.progress + 1)
+                    }
+                }
+            }
+        })
+        lifecycleScope.launch {
+            when (blockThemeManager.getTheme()) {
+                BlockColorTheme.THEME_MONALISA -> binding.radioTheme2.isChecked = true
+                BlockColorTheme.THEME_BELAFONTE -> binding.radioTheme3.isChecked = true
+                BlockColorTheme.THEME_ESPRESSO -> binding.radioTheme4.isChecked = true
+                BlockColorTheme.THEME_SPECTRUM -> binding.radioTheme5.isChecked = true
+                BlockColorTheme.THEME_RAINBOW -> binding.radioTheme6.isChecked = true
+                else -> binding.radioTheme1.isChecked = true
+            }
+        }
+        binding.radioTheme1.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_MODERN)
+            }
+            binding.radioTheme1.isChecked = true
+        }
+        binding.radioTheme2.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_MONALISA)
+            }
+            binding.radioTheme2.isChecked = true
+        }
+        binding.radioTheme3.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_BELAFONTE)
+            }
+            binding.radioTheme3.isChecked = true
+        }
+        binding.radioTheme4.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_ESPRESSO)
+            }
+            binding.radioTheme4.isChecked = true
+        }
+        binding.radioTheme5.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_SPECTRUM)
+            }
+            binding.radioTheme5.isChecked = true
+        }
+        binding.radioTheme6.setOnClickListener {
+            lifecycleScope.launch {
+                blockThemeManager.setTheme(BlockColorTheme.THEME_RAINBOW)
+            }
+            binding.radioTheme6.isChecked = true
+        }
+    }
+}
+
