2fde3a10884bdca6e6fb2fb218257572ba535178
kingzainab <zainabasif14@gmail.com>
2024-12-19 18:25:36 +0500

    GRADLE FIXEDDD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+}
+
+buildscript {
+    val kotlinVersion = "1.8.10" // Kotlin version updated for compatibility
+    dependencies {
+        classpath(libs.gradle) // Gradle plugin version
+        classpath(libs.kotlin.gradle.plugin)
+    }
+}
+
+allprojects {
+    // Remove repository definitions here, as they're already handled in settings.gradle.kts
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
