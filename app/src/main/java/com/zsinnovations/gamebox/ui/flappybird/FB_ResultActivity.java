9c9d2f85f7e112e7551e567e93827c4fd6f9d9b2
saad-1719 <chsaad1407@gmail.com>
2024-12-21 14:02:26 +0500

    Minimal UI updates in the FlappyBird Game

--- app/src/main/java/com/zsinnovations/gamebox/ui/flappybird/FB_ResultActivity.java
+++ app/src/main/java/com/zsinnovations/gamebox/ui/flappybird/FB_ResultActivity.java
@@ -1,4 +1,5 @@
 package com.zsinnovations.gamebox.ui.flappybird;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -16,14 +17,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.zsinnovations.gamebox.MainActivity;
 import com.zsinnovations.gamebox.R;
+import com.zsinnovations.gamebox.ui.balloonburst.BG_GameActivity;
+import com.zsinnovations.gamebox.ui.balloonburst.BG_MainActivity;
+import com.zsinnovations.gamebox.ui.balloonburst.BG_ResultActivity;
 
 public class FB_ResultActivity extends AppCompatActivity {
     private TextView textViewResultInfo, textViewMyScore, textViewHighScore;
-    private Button buttonAgain;
+    private Button buttonAgain,buttonHome;
     private int score, highScore;
 
     private SharedPreferences sharedPreferences;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +45,7 @@ public class FB_ResultActivity extends AppCompatActivity {
         textViewMyScore = findViewById(R.id.textViewMyScore);
         textViewResultInfo = findViewById(R.id.textViewResultInfo);
         buttonAgain = findViewById(R.id.buttonAgain);
+        buttonHome = findViewById(R.id.buttonHome);
 
         // Get the score from the intent
         score = getIntent().getIntExtra("score", 0);
@@ -75,28 +81,43 @@ public class FB_ResultActivity extends AppCompatActivity {
             finish();
         });
 
-        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                showExitConfirmationDialog();
-            }
-        });
-    }
-    private void showExitConfirmationDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(FB_ResultActivity.this);
-        builder.setTitle("Flappy Bird 🐥");
-        builder.setMessage("Are you sure you want to quit the game?");
-        builder.setCancelable(false);
+//        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+//            @Override
+//            public void handleOnBackPressed() {
+//                showExitConfirmationDialog();
+//            }
+//        });
 
-        builder.setNegativeButton("Quit", (dialog, which) -> {
-            onPause();
-            Intent intent = new Intent(FB_ResultActivity.this, MainActivity.class); // Replace with your main activity class
+        buttonAgain.setOnClickListener(v -> {
+            Intent intent = new Intent(FB_ResultActivity.this, FB_GameActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
             finish();
         });
 
-        builder.setPositiveButton("Cancel", (dialog, which) -> dialog.cancel());
-
-        builder.create().show();
+        // Quit Game Button
+        buttonHome.setOnClickListener(v -> {
+            Intent intent = new Intent(FB_ResultActivity.this, FB_MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
     }
+//    private void showExitConfirmationDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(FB_ResultActivity.this);
+//        builder.setTitle("Flappy Bird 🐥");
+//        builder.setMessage("Are you sure you want to quit the game?");
+//        builder.setCancelable(false);
+//
+//        builder.setNegativeButton("Quit", (dialog, which) -> {
+//            onPause();
+//            Intent intent = new Intent(FB_ResultActivity.this, MainActivity.class); // Replace with your main activity class
+//            startActivity(intent);
+//            finish();
+//        });
+//
+//        builder.setPositiveButton("Cancel", (dialog, which) -> dialog.cancel());
+//
+//        builder.create().show();
+//    }
 }
